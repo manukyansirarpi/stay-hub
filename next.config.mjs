8e978856bd8c3886f4a4d34c45756de41fa4745c
Sirarpi Manukyan <manukyansirarpi@gmail.com>
2024-11-22 15:54:42 +0400

    webhook secret

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,10 @@ const nextConfig = {
 
     STRIPE_SECRET_KEY:
       "sk_test_51QCy8a04wfBYPN36bxWpPMqR3KUCM6mfaIvxf8wEDWS6cjF9v4snAgdsE7PhjVsFk8Bg2ijIDsrer2S8QwbuiWz6002jIbUubM",
-    STRIPE_WEBHOOK_SECRET:
-      "whsec_cb2990f493dccca84865d35e44e76475ccd0a4aae5c5b02d22c8229c701f36f5",
+    // STRIPE_WEBHOOK_SECRET:
+    //   "whsec_cb2990f493dccca84865d35e44e76475ccd0a4aae5c5b02d22c8229c701f36f5",
+
+    STRIPE_WEBHOOK_SECRET: "whsec_ati51bQuvpmoMModvHVvpZa3dcg0asZI",
 
     NEXTAUTH_URL: "http://localhost:3000",
     NEXTAUTH_SECRET: "KSDFJKLSDJFLKSDFJSLDKF934KJLDJGDLKGFJDF",
