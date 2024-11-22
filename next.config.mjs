0d5d63787b9a124b90440e5ff2b423df45609dcd
Sirarpi Manukyan <manukyansirarpi@gmail.com>
2024-11-22 16:43:03 +0400

    strype secret key

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,13 @@ const nextConfig = {
   env: {
     // API_URL: "http://localhost:3000/",
     // NEXTAUTH_URL: "http://localhost:3000",
-    // STRIPE_WEBHOOK_SECRET:
-    //   "whsec_cb2990f493dccca84865d35e44e76475ccd0a4aae5c5b02d22c8229c701f36f5",
     // STRIPE_SECRET_KEY:
-    //   "sk_test_51QCy8a04wfBYPN36bxWpPMqR3KUCM6mfaIvxf8wEDWS6cjF9v4snAgdsE7PhjVsFk8Bg2ijIDsrer2S8QwbuiWz6002jIbUubM",
+    // "whsec_cb2990f493dccca84865d35e44e76475ccd0a4aae5c5b02d22c8229c701f36f5",
 
     API_URL: "https://stay-hub-v.vercel.app",
     NEXTAUTH_URL: "https://stay-hub-v.vercel.app",
     STRIPE_SECRET_KEY:
       "sk_test_51QCy8a04wfBYPN36bxWpPMqR3KUCM6mfaIvxf8wEDWS6cjF9v4snAgdsE7PhjVsFk8Bg2ijIDsrer2S8QwbuiWz6002jIbUubM",
-
-    STRIPE_SECRET_KEY:
-      "whsec_cb2990f493dccca84865d35e44e76475ccd0a4aae5c5b02d22c8229c701f36f5",
 
     DB_URI_LOCAl: "mongodb://localhost:27017/stay-hub",
     DB_URI:
