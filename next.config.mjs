6678fb811f613328575aaac523d310ae3525dead
Sirarpi Manukyan <manukyansirarpi@gmail.com>
2024-11-22 16:19:42 +0400

    update next configs env

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    // API_URL: "http://localhost:3000/",
+    // NEXTAUTH_URL: "http://localhost:3000",
+    // STRIPE_WEBHOOK_SECRET:
+    //   "whsec_cb2990f493dccca84865d35e44e76475ccd0a4aae5c5b02d22c8229c701f36f5",
+
     API_URL: "https://stay-hub-v.vercel.app",
+    NEXTAUTH_URL: "https://stay-hub-v.vercel.app",
+    STRIPE_SECRET_KEY:
+      "sk_test_51QCy8a04wfBYPN36bxWpPMqR3KUCM6mfaIvxf8wEDWS6cjF9v4snAgdsE7PhjVsFk8Bg2ijIDsrer2S8QwbuiWz6002jIbUubM",
+
     DB_URI_LOCAl: "mongodb://localhost:27017/stay-hub",
     DB_URI: "mongodb://127.0.0.1:27017/stay-hub",
 
@@ -9,14 +18,7 @@ const nextConfig = {
     CLOUDINARY_API_KEY: "782399657795877",
     CLOUDINARY_API_SECRET: "1W-ZTgNXX7dyO6Q6kFvDel9iHyU",
 
-    STRIPE_SECRET_KEY:
-      "sk_test_51QCy8a04wfBYPN36bxWpPMqR3KUCM6mfaIvxf8wEDWS6cjF9v4snAgdsE7PhjVsFk8Bg2ijIDsrer2S8QwbuiWz6002jIbUubM",
-    // STRIPE_WEBHOOK_SECRET:
-    //   "whsec_cb2990f493dccca84865d35e44e76475ccd0a4aae5c5b02d22c8229c701f36f5",
-
     STRIPE_WEBHOOK_SECRET: "whsec_ati51bQuvpmoMModvHVvpZa3dcg0asZI",
-
-    NEXTAUTH_URL: "https://stay-hub-v.vercel.app",
     NEXTAUTH_SECRET: "KSDFJKLSDJFLKSDFJSLDKF934KJLDJGDLKGFJDF",
   },
   images: {
